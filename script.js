3a1b3a768a20f6ed48a6f60c7705a57698c738fb
Ata Ollah Vaghef Far <a.vagheffar@gmail.com>
2025-06-14 20:24:45 +0330

    dynamic weather icon for all + emoji

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const apiKey = "e7c69f18a3fd68970857543278eeb063";
 const city = "Tehran";
 
-// اطلاعات الان
+// current data
 fetch(
   `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fa`
 )
   .then((res) => res.json())
   .then((data) => {
-    // document.getElementById("ata").textContent = data.main.humidity;
     document.getElementById("temp").textContent = parseInt(data.main.temp);
-    document.getElementById("temp").style.fontSize = "80px";
+    document.getElementById("temp").style.fontSize = "70px";
     document.getElementById("temp").style.fontWeight = "600";
-    // document.getElementById("temp").after.conte = "°";
-    // °
+
     document.getElementById("humidity").textContent = data.main.humidity;
     document.getElementById("wind").textContent = data.wind.speed;
-    // document.getElementById("desc").textContent = data.weather[0].description;
-    // ************************************************************
-    document.getElementById("pic").src = `./pic/sunny_color.png`;
-    document.getElementById("pic").style.width = "100px";
-    document.getElementById("pic").style.height = "100px";
+
+    // ************* new weather pic
+    let mainWeather = data.weather[0].main; // مثلا "Rain"
+    let description = data.weather[0].description; // مثلا "بارش باران متوسط"
+    let weatherId = data.weather[0].id;
+    console.log("weatherId", weatherId);
+    console.log(mainWeather, description);
+    console.log("------------------------");
+
+    let iconPath = "";
+
+    switch (mainWeather) {
+      case "Clear":
+        iconPath = "./pic/sunny_color.png";
+        break;
+      case "Clouds":
+        iconPath = "./pic/cloud_color.png";
+        break;
+      case "Rain":
+        iconPath = "./pic/wet_color.png";
+        break;
+      case "Snow":
+        iconPath = "./pic/snow_color.png";
+        break;
+      default:
+        iconPath = "./pic/full_moon_color.png";
+    }
+    document.getElementById("weatherPic").src = iconPath;
+    document.getElementById("weatherDesc").innerText = description;
+    document.getElementById("weatherPic").style.width = "100px";
+    document.getElementById("weatherPic").style.height = "100px";
     // ************************************************************
     document.getElementById("cityName").textContent = data.name;
     document.getElementById("cityName2").textContent = data.name;
@@ -31,7 +55,7 @@ fetch(
     );
   });
 
-// پیش‌بینی ۵ روز آینده
+// 5 days forecast
 fetch(
   `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=fa`
 )
@@ -48,15 +72,40 @@ fetch(
         weekday: "short",
       });
 
-      const iconUrl = `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
+      let weatherDescDown = data.list[0].weather[0].main;
+      console.log("weatherDescDown", weatherDescDown);
 
+      let weatherDesc2 = data.list[0].weather[0].description;
+      console.log("weatherDesc2", weatherDesc2);
+
+      let iconUrl = "";
+      switch (weatherDescDown) {
+        case "Clear":
+          iconUrl = "./pic/sunny_color.png";
+          break;
+        case "Clouds":
+          iconUrl = "./pic/cloud_color.png";
+          break;
+        case "Rain":
+          iconUrl = "./pic/wet_color.png";
+          break;
+        case "Snow":
+          iconUrl = "./pic/snow_color.png";
+          break;
+        default:
+          iconUrl = "./pic/full_moon_color.png";
+      }
+
+      //----------------------------------------
       const div = document.createElement("div");
       div.className = "day";
       div.innerHTML = `
         <img src="${iconUrl}" width="48">
         <p>${Math.round(item.main.temp)}°</p>
+        <p>${weatherDesc2}</p>
         <p>${dayName}</p>
       `;
       forecastContainer.appendChild(div);
+      console.log("+++++++");
     });
   });
